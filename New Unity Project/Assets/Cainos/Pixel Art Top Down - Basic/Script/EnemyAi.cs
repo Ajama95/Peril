3e7ebee7e814a66dc7f3732224f117972c58d287
Ajama95 <54416686+Ajama95@users.noreply.github.com>
2021-05-07 20:11:01 +0100

    ai-nearly done

---
+++ New Unity Project/Assets/Cainos/Pixel Art Top Down - Basic/Script/EnemyAi.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAi : MonoBehaviour
+{
+    public bool Patroling;
+    private bool turn;
+
+    public float speed;
+
+    Rigidbody2D rb;
+
+    public Transform p1;
+    public Transform p2;
+    public LayerMask groundlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Patroling = true;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Patroling)
+        {
+            Patrol();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(Patroling == true)
+        {
+            turn = !Physics2D.OverlapCircle(p1.position, 0.1f, groundlayer);
+        }
+    }
+
+    void Patrol()
+    {
+        if(turn)
+        {
+            flip();
+        }
+        rb.velocity = new Vector2(speed * Time.fixedDeltaTime, rb.velocity.y);
+
+    }
+    void flip()
+    {   Patroling = false;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        speed *= -1;
+        Patroling = true;
+    }
+
+
+
+
+}
